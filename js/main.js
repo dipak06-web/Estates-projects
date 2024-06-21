4530398d712d6c359bb339c91546a564b6b8d7a2
mac <mac@macs-MacBook-Pro-6.local>
2024-06-21 23:44:52 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$('.mid-box  .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+
+
+
+
+
+
+$('#developer  .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
